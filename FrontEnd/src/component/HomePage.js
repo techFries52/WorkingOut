0846b59f10092b7e9801f18c5d1f1e5b508d28bd
Jesse <69030071+techFries52@users.noreply.github.com>
2021-04-24 11:23:37 -0500

    ye

--- FrontEnd/src/component/HomePage.js
+++ FrontEnd/src/component/HomePage.js
@@ -3,201 +3,200 @@ import Exercises from "./Exercises";
 import Selection from "./Selection";
 import { toast } from "react-toastify";
 
-
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        workouts: [
-          {
-            _id: "00",
-            title: "-- Hydrate! --",
-            reps: "2x"
-          },
-          {
-            _id: "0",
-            title: "Flat Bench Press",
-            reps: "10x"
-          },
-          {
-            _id: "1",
-            title: "Tricep Bench Press",
-            reps: "10x"
-          },
-          {
-            _id: "2",
-            title: "Incline Bench Press",
-            reps: "10x"
-          },
-          {
-            _id: "3",
-            title: "Dumbell Fly",
-            reps: "10x"
-          },
-          {
-            _id: "4",
-            title: "Push Ups",
-            reps: "10x"
-          },
-          {
-            _id: "5",
-            title: "Pull Ups",
-            reps: "10x"
-          },
-          {
-            _id: "6",
-            title: "Single Arm Row",
-            reps: "10x"
-          },
-          {
-            _id: "7",
-            title: "Bent Over Barbell Row",
-            reps: "10x"
-          },
-          {
-            _id: "8",
-            title: "Incline Row",
-            reps: "10x"
-          },
-          {
-            _id: "9",
-            title: "Shoulder Press",
-            reps: "10x"
-          },
-          {
-            _id: "10",
-            title: "Front Deltoid Raise",
-            reps: "10x"
-          },
-          {
-            _id: "11",
-            title: "Lateral Deltoid Raise",
-            reps: "10x"
-          },
-          {
-            _id: "12",
-            title: "Rear Deltoid Raise",
-            reps: "10x"
-          },
-          {
-            _id: "13",
-            title: "Shoulder Shrugs",
-            reps: "10x"
-          },
-          {
-            _id: "14",
-            title: "Bicep Curl",
-            reps: "10x"
-          },
-          {
-            _id: "15",
-            title: "Hammer Curl",
-            reps: "10x"
-          },
-          {
-            _id: "16",
-            title: "Reverse Curl",
-            reps: "10x"
-          },
-          {
-            _id: "17",
-            title: "Skull Crushers",
-            reps: "10x"
-          },
-          {
-            _id: "18",
-            title: "Standing Tricep Extension",
-            reps: "10x"
-          },
-          {
-            _id: "19",
-            title: "Bent Over Tricep ",
-            reps: "10x"
-          },
-          {
-            _id: "20",
-            title: "Body Squat",
-            reps: "10x"
-          },
-          {
-            _id: "21",
-            title: "Weighted Squat",
-            reps: "10x"
-          },
-          {
-            _id: "22",
-            title: "Lunges",
-            reps: "10x"
-          },
-          {
-            _id: "23",
-            title: "Step Ups",
-            reps: "10x"
-          },
-          {
-            _id: "24",
-            title: "Straight Leg Deadligt",
-            reps: "10x"
-          },
-          {
-            _id: "25",
-            title: "Calf Raises",
-            reps: "10x"
-          },
-          {
-            _id: "26",
-            title: "Hanging Leg Raises",
-            reps: "10x"
-          }
-        ],
-        selectedWorkouts: []
+      workouts: [
+        {
+          _id: "00",
+          title: "-- Hydrate! --",
+          reps: "2x",
+        },
+        {
+          _id: "0",
+          title: "Flat Bench Press",
+          reps: "10x",
+        },
+        {
+          _id: "1",
+          title: "Tricep Bench Press",
+          reps: "10x",
+        },
+        {
+          _id: "2",
+          title: "Incline Bench Press",
+          reps: "10x",
+        },
+        {
+          _id: "3",
+          title: "Dumbell Fly",
+          reps: "10x",
+        },
+        {
+          _id: "4",
+          title: "Push Ups",
+          reps: "10x",
+        },
+        {
+          _id: "5",
+          title: "Pull Ups",
+          reps: "10x",
+        },
+        {
+          _id: "6",
+          title: "Single Arm Row",
+          reps: "10x",
+        },
+        {
+          _id: "7",
+          title: "Bent Over Barbell Row",
+          reps: "10x",
+        },
+        {
+          _id: "8",
+          title: "Incline Row",
+          reps: "10x",
+        },
+        {
+          _id: "9",
+          title: "Shoulder Press",
+          reps: "10x",
+        },
+        {
+          _id: "10",
+          title: "Front Deltoid Raise",
+          reps: "10x",
+        },
+        {
+          _id: "11",
+          title: "Lateral Deltoid Raise",
+          reps: "10x",
+        },
+        {
+          _id: "12",
+          title: "Rear Deltoid Raise",
+          reps: "10x",
+        },
+        {
+          _id: "13",
+          title: "Shoulder Shrugs",
+          reps: "10x",
+        },
+        {
+          _id: "14",
+          title: "Bicep Curl",
+          reps: "10x",
+        },
+        {
+          _id: "15",
+          title: "Hammer Curl",
+          reps: "10x",
+        },
+        {
+          _id: "16",
+          title: "Reverse Curl",
+          reps: "10x",
+        },
+        {
+          _id: "17",
+          title: "Skull Crushers",
+          reps: "10x",
+        },
+        {
+          _id: "18",
+          title: "Standing Tricep Extension",
+          reps: "10x",
+        },
+        {
+          _id: "19",
+          title: "Bent Over Tricep ",
+          reps: "10x",
+        },
+        {
+          _id: "20",
+          title: "Body Squat",
+          reps: "10x",
+        },
+        {
+          _id: "21",
+          title: "Weighted Squat",
+          reps: "10x",
+        },
+        {
+          _id: "22",
+          title: "Lunges",
+          reps: "10x",
+        },
+        {
+          _id: "23",
+          title: "Step Ups",
+          reps: "10x",
+        },
+        {
+          _id: "24",
+          title: "Straight Leg Deadligt",
+          reps: "10x",
+        },
+        {
+          _id: "25",
+          title: "Calf Raises",
+          reps: "10x",
+        },
+        {
+          _id: "26",
+          title: "Hanging Leg Raises",
+          reps: "10x",
+        },
+      ],
+      selectedWorkouts: [],
     };
   }
 
   workoutSelect = (exercise) => {
-    this.setState(prevState => ({
-       selectedWorkouts: [...prevState.selectedWorkouts, exercise],
-      
-    }))
-  }  
+    this.setState((prevState) => ({
+      selectedWorkouts: [...prevState.selectedWorkouts, exercise],
+    }));
+  };
 
   workoutDeselect = (exercise) => {
-    this.setState(state => {
+    this.setState((state) => {
       const selectedWorkouts = [...state.selectedWorkouts];
-      selectedWorkouts.splice(selectedWorkouts.indexOf(exercise.title))
-      return ({
-        selectedWorkouts
-      })
-      
-    })
-  }
+      selectedWorkouts.splice(selectedWorkouts.indexOf(exercise.title));
+      return {
+        selectedWorkouts,
+      };
+    });
+  };
 
   workoutCompleted = (exercise) => {
-    this.setState(state => {
-      const selectedWorkouts = [...state.selectedWorkouts];      
-      toast.success("Exercise Completed!")
-      selectedWorkouts.splice(selectedWorkouts.indexOf(exercise.title))
-      return ({
-        selectedWorkouts
-      });
-      
-    })
-  }
+    this.setState((state) => {
+      console.log(exercise);
+      const selectedWorkouts = [...state.selectedWorkouts];
+      toast.success("Exercise Completed!");
+      selectedWorkouts.splice(selectedWorkouts.indexOf(exercise.title));
+      return {
+        selectedWorkouts,
+      };
+    });
+  };
 
   render() {
     const { workouts, selectedWorkouts } = this.state;
-  
-    return(
+
+    return (
       <div>
-        
         <div className="container-fluid">
           <div className="row justify-content-center">
             <Exercises workouts={workouts} workoutSelect={this.workoutSelect} />
-            <Selection selectedWorkouts={selectedWorkouts} workoutDeselect={this.workoutDeselect} workoutCompleted={this.workoutCompleted} />      
+            <Selection
+              selectedWorkouts={selectedWorkouts}
+              workoutDeselect={this.workoutDeselect}
+              workoutCompleted={this.workoutCompleted}
+            />
           </div>
         </div>
       </div>
-            
-    )
+    );
   }
 }
--- FrontEnd/src/component/Selection.js
+++ FrontEnd/src/component/Selection.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React from "react";
 import uuid from "react-uuid";
 
 const Selection = (props) => {
-    const { selectedWorkouts, workoutDeselect, workoutCompleted } = props;    
-    
-    return (
-        <div className="col-md-4">
-            <h1>Reps | Selection <i className="fa fa-plus"></i></h1>
-            <ul className="eList">
-                { selectedWorkouts.map(exercise => {
-                    return <li key={uuid()}><span className="repsLi">{exercise.reps}</span><span onClick={() => workoutCompleted(exercise)} className="compSp"><i className="fa fa-check"></i></span>{exercise.title}<span onClick={() => workoutDeselect(exercise)} className="float-right"><i className="fa fa-trash"></i></span></li>
-                })}                   
-            </ul>                
-        </div>
-    )
-}
-export default Selection;
+  const { selectedWorkouts, workoutDeselect, workoutCompleted } = props;
 
+  return (
+    <div className="col-md-4">
+      <h1>
+        Reps | Selection <i className="fa fa-plus"></i>
+      </h1>
+      <ul className="eList">
+        {selectedWorkouts.map((exercise) => {
+          return (
+            <li key={uuid()}>
+              <span className="repsLi">{exercise.reps}</span>
+              <span
+                onClick={() => workoutCompleted(exercise)}
+                className="compLi"
+              >
+                <i className="fa fa-check"></i>
+              </span>
+              {exercise.title}
+              <span
+                onClick={() => workoutDeselect(exercise)}
+                className="float-right trash"
+              >
+                <i className="fa fa-trash"></i>
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+export default Selection;
